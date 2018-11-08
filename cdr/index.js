42009194b6889737ef5b836ff003442a6b221015
Mathew Benson <mbenson@windhoverlabs.com>
2018-11-08 09:54:10 -0600

    Adding radar display to cdr plugin

--- cdr/index.js
+++ cdr/index.js
@@ -20,48 +20,24 @@ module.exports = class CfeCdrPlugin extends CdrPlugin {
 					longDescription: 'Main AMC.',
 					filePath: '/main_layout.lyt'
 				},
-				appctrl: {
-					type: CdrPlugin.ContentType.PANEL,
-					shortDescription: 'Application Control',
-					longDescription: 'Application start, stop, restart, and reload.',
-					filePath: '/appctrl.pug'
-				},
 				cdh: {
 					type: CdrPlugin.ContentType.PANEL,
 					shortDescription: 'Command and Data Handling',
 					longDescription: 'Command counters.',
 					filePath: '/cdh.pug'
 				},
-				tc: {
-					type: CdrPlugin.ContentType.PANEL,
-					shortDescription: 'Table Control',
-					longDescription: 'Table Control',
-					filePath: '/tc.pug'
-				},
-                ef: {
-                    type: CdrPlugin.ContentType.PANEL,
-                    shortDescription: 'Event Filter',
-                    longDescription: 'Event Filter',
-                    filePath: '/ef.pug'
-                },
-                sch: {
-                    type: CdrPlugin.ContentType.PANEL,
-                    shortDescription: 'Scheduler',
-                    longDescription: 'Scheduler',
-                    filePath: '/sch.pug'
-                },
-                tlmc: {
-                    type: CdrPlugin.ContentType.PANEL,
-                    shortDescription: 'Telemetry Control',
-                    longDescription: 'Telemetry Control',
-                    filePath: '/tlmc.pug'
-                },
                 ao: {
                     type: CdrPlugin.ContentType.PANEL,
                     shortDescription: 'Actuator Outputs',
                     longDescription: 'Actuator Outputs',
                     filePath: '/ao.pug'
                 },
+                radarjs: {
+                    filePath: '/radar.js'
+                },
+                radarcss: {
+                    filePath: '/radar.css'
+                }
       }
     };
 
